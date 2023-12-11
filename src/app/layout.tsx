e739871af716241210d394cbffd75279a89093fa
Mounir <lm_melzi@esi.dz>
2023-12-11 15:53:54 +0100

    add form ui components from shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html suppressHydrationWarning lang="en">
+      <body suppressHydrationWarning className={inter.className}>
+        {children}
+      </body>
     </html>
   );
 }
