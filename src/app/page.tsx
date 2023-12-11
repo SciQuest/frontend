6ba430364f69e888eb08414bb27b48021cbb1736
Mounir <lm_melzi@esi.dz>
2023-12-11 14:51:21 +0100

    add shadcn ui to the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <section className="flex w-full h-full items-center justify-center">
-      Welcome to SciQuest
+      <Button>Welcome to SciQuest</Button>
     </section>
   );
 }
