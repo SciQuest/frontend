b31b5b16f66914feca9040f3447b958f4a44ecb9
Mounir <lm_melzi@esi.dz>
2024-02-03 16:42:52 +0100

    update routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+import React from "react";
 import { ProtectedComponent } from "@/lib/auth";
+import AboutUs from "@/app/home/AboutUs";
+import Hero from "@/app/home/Hero";
+import Footer from "@/app/home/Footer";
+import Navbar from "@/components/Navbar";
 
-export default function Home() {
+const Home = () => {
   return (
-    <ProtectedComponent roles={["ADMIN", "MODERATOR", "USER"]}>
-      <section className="flex w-full h-full items-center justify-center">
-        Welcome to SciQuest
-      </section>
+    <ProtectedComponent roles={["USER"]}>
+      <div>
+        <Navbar />
+        <Hero />
+        <AboutUs />
+        <Footer />
+      </div>
     </ProtectedComponent>
   );
-}
+};
+
+export default Home;
